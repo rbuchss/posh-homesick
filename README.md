6fe946184e7baf1e4635f2b23b5b956ccd061f70
rbuchss <rbuchss@gmail.com>
2020-03-02 16:08:55 -0500

    convert into powershell module

---
+++ README.md
@@ -0,0 +1,8 @@
+homewick
+==========
+A powershell port of homesick/homeshick for managing configuration files in windows powershell
+
+# Quick Install
+```powershell
+Import-Module homewick
+```
---
+++ homewick.psm1
@@ -0,0 +1,111 @@
+<#
+  Wrapper for homewick task invoking
+#>
+function Invoke-Homewick {
+  [CmdletBinding()]
+  param (
+    [Parameter(
+      Mandatory = $true,
+      Position = 0,
+      HelpMessage = 'yipp!'
+    )]
+    [ValidateSet(
+      'cd',
+      'clone',
+      'generate',
+      'help',
+      'link',
+      'list',
+      'open',
+      'pull',
+      'push',
+      'unlink',
+      ErrorMessage = "Value '{0}' is invalid. Try one of: '{1}'"
+    )]
+    [string]
+    $Task,
+
+    [Parameter(Position=1, ValueFromRemainingArguments)]
+    [string[]]
+    $Arguments
+  )
+  switch ($Task) {
+    'cd' { Set-HomewickLocation -Path $Arguments[0] }
+    'clone' { Get-HomewickClone -URL $Arguments[0] }
+    'generate' { throw 'not implemented!' }
+    'help' { Show-HomewickHelp }
+    'generate' { throw 'not implemented!' }
+    'link' { throw 'not implemented!' }
+    'list' { throw 'not implemented!' }
+    'open' { throw 'not implemented!' }
+    'pull' { throw 'not implemented!' }
+    'push' { throw 'not implemented!' }
+    'unlink' { throw 'not implemented!' }
+    Default { throw }
+  }
+}
+
+function Set-HomewickLocation {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory)]
+    [string]
+    $Path
+  )
+  Write-Host "cd $Path"
+}
+
+function Get-HomewickClone {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory)]
+    [string]
+    $URL
+  )
+  Write-Host "clone $URL"
+}
+
+function Show-HomewickHelp {
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [string]
+    $Task
+  )
+  Write-Host "Help"
+}
+
+<#
+  Creates symlinks for all files in repo home dir in $HOME
+#>
+
+  # $repoPath = (Get-Item $PSScriptRoot).Parent
+  # $repoName = $repoPath.BaseName
+
+  # $repoHome = if (Test-Path (Join-Path $repoPath "windows-home")) {
+  #   (Join-Path $repoPath "windows-home")
+  # } elseif (Test-Path (Join-Path $repoPath "home")) {
+  #   (Join-Path $repoPath "home")
+  # } else {
+  #   throw "Repo has neither 'home' nor 'windows-home' directories required for linking!"
+  # }
+
+  # $force = $true
+
+  # Write-Host "linking home files from '$repoName' ..." -ForegroundColor Yellow
+
+  # Get-ChildItem $repoHome |
+  # Foreach-Object {
+  #   $linkPath = "$HOME\$($_.Name)"
+
+  #   if (Test-Path $linkPath) {
+  #     Write-Host "exists:`t`t'$linkPath' -> '$_'" -ForegroundColor Blue 
+  #     if ($force) {
+  #       Write-Host "overwrite?`t'$linkPath' -> '$_'" -ForegroundColor Red
+  #       New-Item -ItemType SymbolicLink -Path $HOME -Name $_.Name -Value $_ -Confirm -Force > $null
+  #     }
+  #   } else {
+  #     Write-Host "linking:`t`t'$linkPath' -> '$_'" -ForegroundColor Green
+  #     New-Item -ItemType SymbolicLink -Path $HOME -Name $_.Name -Value $_ > $null
+  #   }
+  # }
