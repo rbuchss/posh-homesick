53c32b0acf73a5eecd2c552a1723c91f447ee08c
rbuchss <rbuchss@gmail.com>
2020-03-06 17:17:45 -0500

    enable verbose install flag

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $fal
 $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 if ($Force -and (Get-Module homewick)) {
-  Remove-Module homewick
+  Remove-Module homewick -Force -Verbose:$Verbose
 }
 
-Import-Module $installDir\src\homewick.psd1
+Import-Module $installDir\src\homewick.psd1 -Force:$Force -Verbose:$Verbose
