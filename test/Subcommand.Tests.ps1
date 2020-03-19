5bd46de683dc9ac06bfb80e392f37ebf0e33490f
rbuchss <rbuchss@gmail.com>
2020-03-19 17:26:53 -0400

    convert subcommand tests to Pester 4.x assertions

--- test/Subcommand.Tests.ps1
+++ test/Subcommand.Tests.ps1
@@ -2,37 +2,37 @@ using module '..\src\posh-homesick\Subcommand.psm1'
 
 Describe 'Subcommand' {
   It 'Has valid values' {
-    [Subcommand]::validValues.Count | Should Be 10
+    [Subcommand]::validValues | Should -HaveCount 10
   }
 
   Context '.Select' {
     It 'Returns all values with no filter' {
-      [Subcommand]::Select($null) | Should Be @([Subcommand]::validValues)
+      [Subcommand]::Select($null) | Should -Be @([Subcommand]::validValues)
     }
 
     It 'Returns all values matching filter' {
-      [Subcommand]::Select('c') | Should Be @('cd', 'clone')
-      [Subcommand]::Select('op') | Should Be @('open')
+      [Subcommand]::Select('c') | Should -Be @('cd', 'clone')
+      [Subcommand]::Select('op') | Should -Be @('open')
     }
 
     It 'Returns null if no values match the filter' {
-      [Subcommand]::Select('does-not-exist') | Should Be $null
+      [Subcommand]::Select('does-not-exist') | Should -BeNull
     }
   }
 
   Context '.IsValidHelpValue' {
     It 'Returns true for a blank value' {
-      [Subcommand]::IsValidHelpValue('') | Should Be $true
+      [Subcommand]::IsValidHelpValue('') | Should -BeTrue
     }
 
     It 'Returns true if value is a valid value' {
-      [Subcommand]::IsValidHelpValue('cd') | Should Be $true
-      [Subcommand]::IsValidHelpValue('open') | Should Be $true
+      [Subcommand]::IsValidHelpValue('cd') | Should -BeTrue
+      [Subcommand]::IsValidHelpValue('open') | Should -BeTrue
     }
 
     It 'Returns false if value is not blank or not a valid value' {
-      [Subcommand]::IsValidHelpValue('does-not-exist') | Should Be $false
-      [Subcommand]::IsValidHelpValue($null) | Should Be $false
+      [Subcommand]::IsValidHelpValue('does-not-exist') | Should -BeFalse
+      [Subcommand]::IsValidHelpValue($null) | Should -BeFalse
     }
   }
 }
